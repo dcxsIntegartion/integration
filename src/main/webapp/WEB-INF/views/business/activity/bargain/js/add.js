01a4eeccbbfa9ae93b2e045e72e016ab703cbc0d
hasee <hasee@192.168.1.103>
2017-05-10 23:08:45 +0800

    特价活动前端

--- src/main/webapp/WEB-INF/views/business/activity/bargain/js/add.js
+++ src/main/webapp/WEB-INF/views/business/activity/bargain/js/add.js
@@ -1,9 +1,10 @@
 var id = getQueryString("id");
 var type = getQueryString("type");
-var view_url = basePath+"/bis/mach/";
+var view_url = basePath+"/bis/activityBargain/";
 /** 页面数据封装 填充 **/
 var model ={};
-var field = ["id","machName","machImg","machIndro","machDress","machLng","machLat"];
+var field = ["activityName","activityStartTime","activityEndTime","activityStoreId",
+	"activityExplain","shareTitle","shareDescribe", "activityPic", "sharePic"];
 var modelUtils = new ModelUtils(field);
 /** 提交按钮 **/
 var submitHtml = $('#saveBtn').html();
@@ -44,7 +45,8 @@ $(function(){
 			submitToggle(1,submitHtml);
 			var model = modelUtils.initModel();
 			/** 上传图片字段赋值 **/
-			model.machImg=saveRel(machImg);
+			model.activityPic=saveRel(activityPic);
+			model.sharePic=saveRel(sharePic);
 			/** 所在区域赋值 **/
 			model.machArea=selectedcityfullname;
 			model.machAreaCode=selectedcityCode;
@@ -86,26 +88,102 @@ function submitToggle(status, submitHtml) {
 }
 /** 页面数据加载控制器 **/
 function viewLoadControlle(){
-	/** 加载数据 **/
-	if(type=="view" || type=="update"){
-		if(type=="view")submitToggle(2,submitHtml);
-		$.ajax({
-	        type: "post",
-	        async:false,
-	        url:  view_url+"view?id="+id,
-	        success: function(data){
-	        	if(data.state==1){
-	        		$("#areaCode").val(data.data.machAreaCode);
-	        		modelUtils.fillData(data.data);
-	        	}
-			},
-			error:function(data) {
-			}
-	    });
+/** 加载数据 **/
+if(type=="view" || type=="update"){
+	if(type=="view")submitToggle(2,submitHtml);
+	$.ajax({
+        type: "post",
+        async:false,
+        url:  view_url+"view?id="+id,
+        success: function(data){
+        	if(data.state==1){
+        		$("#areaCode").val(data.data.machAreaCode);
+        		modelUtils.fillData(data.data);
+        	}
+		},
+		error:function(data) {
+		}
+    });
+}
+
+var selectedCommodities = [];//已选择的商品列表
+
+var gridObj;//商品选择列表
+var gridObj2;//已选择商品列表
+/***更新商品列表****/
+function updateCommidities(){
+	gridObj = $.fn.bsgrid.init('searchTable', {
+		 url : view_url+"获取未选择的商品列表",
+        pageSizeSelect: true,
+        stripeRows: true,
+        otherParames: {
+        	selectedCommodities:selectedCommodities
+        },
+        pageSize: 10,
+        pagingLittleToolbar:false
+   });
+	gridObj2 = $.fn.bsgrid.init('selectedTable', {
+		localData: selectedCommodities,
+		pageSize: 10,
+		pagingLittleToolbar:false
+	});
+}
+/** 列表操作按钮 **/
+function operate(record, rowIndex, colIndex, options) {
+	var op_html ='';
+//	var state = record.mach_state;
+	op_html+= '<a  href="javascript:void(0);" class="btn btn-outline btn-primary btn-xs mhx" onclick="selectCommodity(\'' + record + '\');">选择</a>';
+	return op_html;
+};
+function operate2(record, rowIndex, colIndex, options) {
+	var op_html ='';
+//	var state = record.mach_state;
+	op_html+= '<a  href="javascript:void(0);" class="btn btn-outline btn-primary btn-xs mhx" onclick="removeCommodity(\'' + record + '\');">移除</a>';
+	return op_html;
+};
+
+/** 选择商品 **/
+function selectCommodity(record){
+	//将商品加入集合
+	selectedCommodities.push(record);
+	//刷新列表
+	updateCommidities();
+}
+/** 移除商品 **/
+function removeCommodity(record){
+	//迭代删除需要移除的商品
+	var id = record.id;
+	for(var i=0; i<selectedCommodities.length; i++) {
+    if(selectedCommodities[i].id == id) {
+	    	selectedCommodities.splice(i, 1);
+	      break;
+	    }
 	}
-	
-	
-	
+	//刷新列表
+	updateCommidities();
+}
+
+/***门店选择弹出框***/
+$('#activityStoreName').click(function () {
+	layer.open({
+	    type: 2,
+	    title: '门店选择',
+	    shadeClose: true,
+	    maxmin: true, //开启最大化最小化按钮
+	    shade: 0.8,
+	    area: ['500px', '99%'],
+	    content: "../../../commonSelect/storeSelect.html" //iframe的url
+	}); 
+});
+/***门店选择回调函数****/
+function fnLoadStore(storeId,storeName){
+	$("#activityStoreId").val(storeId);
+	$("#activityStoreName").val(storeName);
+	//清空选择的商品列表
+	selectedCommodities=[];
+	//请求/重新请求商品列表
+	updateCommidities();
+}
 	//商品选择步骤：
 	//1·每选择一个弹出输入数量和价格框
 	//2.每选择一次将商品id传入后台刷新列表，将选中的商品、价格、数量显示在选中列表
---
+++ src/main/webapp/WEB-INF/views/business/commonSelect/js/storeSelect.js
@@ -0,0 +1,42 @@
+var grid_url = basePath + "/bis/store/";
+var gridObj;
+/****门店选择操作***/
+function operate(record, rowIndex, colIndex, options) {
+	var storeId = record.id;
+	var storeName = record.storeName;
+	return "<a  href=\"#\" class=\"btn btn-primary btn-xs mhx \" "
+			+ "onclick=\"forBuildChoose('"
+			+ storeId + "','"
+			+ storeName + "')\">选择</a>";
+}
+
+/**
+ * 功能 : 1、选择门店后调用父界面函数fnLoadStore将选择的楼盘信息进行回填 2、关闭该选择界面
+ */
+function forStoreChoose(storeId, storeName) {
+	parent.frames[0].fnLoadStore(storeId, storeName);
+	parent.$.fancybox.close();
+	return false;
+}
+
+function doSearch() {
+	gridObj.options.otherParames = $("#search_build_form").serializeArray();
+	gridObj.page(1);
+}
+
+function resetForm() {
+	$("#search_build_form")[0].reset();
+	gridObj.options.otherParames = $("#search_build_form").serializeArray();
+	gridObj.page(1);
+}
+
+$(function() {
+	gridObj = $.fn.bsgrid.init('searchTable', {
+		url : grid_url + "page",
+		pageSizeSelect : true,
+		stripeRows : true,
+		otherParames : $("#search_form").serializeArray(),
+		pageSize : 10,
+		pagingLittleToolbar : false
+	});
+});
